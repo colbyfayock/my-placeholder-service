8db9f8997839b0720a6ea6d070144a2474042a1d
colbyfayock <fayock@gmail.com>
2022-07-12 15:17:25 -0400

    Step 5: Generating a Cloudinary URL and returning it as a redirect

--- functions/placeholder.js
+++ functions/placeholder.js
@@ -10,8 +10,17 @@ exports.handler = async (event, context) => {
   const paramsPath = path.replace('/.netlify/functions/placeholder', '');
   const [width, height, background] = paramsPath.split('/').filter(param => !!param);
 
+  const url = cloudinary.url('white', {
+    width,
+    height,
+    effect: 'colorize',
+    color: `#${background}`
+  });
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ status: 'Ok' })
+    statusCode: 302,
+    headers: {
+      Location: url
+    }
   }
 };
