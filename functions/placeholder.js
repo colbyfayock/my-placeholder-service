97a448e94b8030917ace999a673e30b8ceaa0d4b
colbyfayock <fayock@gmail.com>
2022-07-12 16:24:23 -0400

    Extra: environment variable

--- functions/placeholder.js
+++ functions/placeholder.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: '<Your Cloud Name>'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME
 });
 
 exports.handler = async (event, context) => {
