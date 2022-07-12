dc3dbcad71b954cc6636067fc283bd90638f3602
colbyfayock <fayock@gmail.com>
2022-07-12 15:16:26 -0400

    Step 3: destructuring event parameters to use for URL generation

--- functions/placeholder.js
+++ functions/placeholder.js
@@ -1,4 +1,9 @@
 exports.handler = async (event, context) => {
+  const { path } = event;
+
+  const paramsPath = path.replace('/.netlify/functions/placeholder', '');
+  const [width, height, background] = paramsPath.split('/').filter(param => !!param);
+
   return {
     statusCode: 200,
     body: JSON.stringify({ status: 'Ok' })
